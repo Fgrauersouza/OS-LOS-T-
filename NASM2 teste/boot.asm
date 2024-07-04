0786d7be4beed9b1d8d99f1467f287949ab3f8ea
Fábio Souza <37919743+Fgrauersouza@users.noreply.github.com>
2024-07-04 19:19:56 -0300

    Pasta NASM2 - Boot correto (recente)
    Na pasta NASM 2 o "boot0.1.asm" é o mais recente trabalhado.

---
+++ NASM2 teste/boot.asm
@@ -0,0 +1,20 @@
+org 0x7c00
+bits 16
+
+main:
+
+        mov al, 0x02
+        mov ah, 0x0e
+        mov bh, 0
+        int 0x10
+
+
+halt:
+        ; Parada da CPU...
+        hlt
+        jmp halt
+
+times 510-($-$$) db 0
+dw 0xaa55
+
+   
---
+++ NASM2 teste/boot0.1.asm
@@ -0,0 +1,25 @@
+org 0x7c00
+bits 16
+
+main:
+
+print_str:
+    mov si, hello
+    mov ah, 0x0e
+.next_char:
+    lodsb
+    or al, al
+    jz halt
+    int 0x10
+    jmp .next_char
+
+halt:
+    cli
+    hlt
+
+hello: db 'Welcome to LOS/T!', 0  
+
+times 510-($-$$) db 0
+dw 0xaa55
+
+   
